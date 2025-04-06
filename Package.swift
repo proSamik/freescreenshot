33dd07978e44546dba69183b7cd524e58e6bb315
proSamik <samikchoudhury15@gmail.com>
2025-04-06 22:24:46 +0530

    Update Package.swift to specify target path for FreeScreenshot

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     targets: [
         .target(
             name: "FreeScreenshot",
-            dependencies: ["HotKey"]),
+            dependencies: ["HotKey"],
+            path: "freescreenshot"),
     ]
 ) 
